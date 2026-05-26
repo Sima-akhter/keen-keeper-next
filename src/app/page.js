781fe79924d081d763b458e52c15bff042f7e4e7
Sima-akhter <simaakter5301@gmail.com>
2026-05-26 23:19:28 +0600

    added app context click btn

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Banner from "@/components/banner/Banner";
+import Card from "@/components/card/Card";
 
 
 export default function Home() {
   return (
     <div>
       <Banner></Banner>
+      <Card></Card>
     </div>
   );
 }
--- src/components/clickbtn/QuickCheckIn.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const QuickCheckIn = () => {
-  return (
-    <div>QuickCheckIn</div>
-  )
-}
-
-export default QuickCheckIn
